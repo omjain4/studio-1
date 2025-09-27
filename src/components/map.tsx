a978af312e8b5b227f97ba777a41885caade8fe9
Om Jain <omjain44444@gmail.com>
2025-09-27 15:51:28 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import dynamic from 'next/dynamic';
 import { Skeleton } from '@/components/ui/skeleton';
 
@@ -7,6 +9,7 @@ const MapView = dynamic(() => import('@/components/map-view'), {
 });
 
 export default function Map() {
+  // NEXT_PUBLIC_GOOGLE_MAPS_API_KEY is now read on the client, so it needs to be exposed as NEXT_PUBLIC_
   const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '';
   return <MapView apiKey={apiKey} />;
 }
